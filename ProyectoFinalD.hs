435fbabf0c68df82de85a6f545d3076352aafd40
Mauro Emiliano <106214079+EmiMau@users.noreply.github.com>
2023-06-07 22:08:18 -0600

    Update ProyectoFinalD.hs
    Ahora es posible llamar al programa en main haciendo unas ligeras variaciones sobre su sintaxis

--- ProyectoFinalD.hs
+++ ProyectoFinalD.hs
@@ -8,7 +8,6 @@ import Data.Text (Text)
 import Data.Void
 import Text.Megaparsec hiding (State)
 import Text.Megaparsec.Char
--- import Control.Monad.Combinators.Expr
 import qualified Text.Megaparsec as M
 import qualified Data.Text as T
 import qualified Text.Megaparsec.Char.Lexer as L
@@ -16,7 +15,7 @@ import qualified Text.Megaparsec.Char.Lexer as L
 type Parser = Parsec Void String
 
 data Expr
-  = Var {var :: Char, rest :: Expr}
+  = Var {var :: Char}
   | Abs {var :: Char, cuerpo :: String}
   | App {var :: Char, cuerpo :: String, apli :: Expr}
   deriving (Eq, Ord, Show)
@@ -48,20 +47,26 @@ pVar :: Parser Expr
 pVar = do
   void (string "")
   var <- alphaNumChar
-  rest <- try pVar <|> pAbs <|> pApp
-  return (Var var rest)
-
+  return (Var var)
 
 reduccion :: Expr -> String
-reduccion Var{var=var,rest=rest} = [var] ++ reduccion rest
+reduccion Var{var=var}= [var]
 reduccion Abs{var=var,cuerpo=cuerpo} = "(\\"++[var]++"."++cuerpo++")"
 reduccion App{var=var,cuerpo=cuerpo,apli=apli} = sust var red cuerpo
   where red = reduccion apli
 
-parserLambda :: Parser Expr
 parserLambda = do 
-  try pVar <|> pAbs <|> pApp
+  c <- try pApp <|> pAbs <|> pVar
+  return (reduccion c)
 
+main :: IO ()
+main = do
+  putStrLn "Introduce tu abstracción Lambda:"
+  --Aquí por los escape tenemos que usar
+  -- la sintaxis de una sóla '\'
+  -- por ejemplo (\x.xyz) w 
+  linea <- getLine
+  parseTest parserLambda linea
 
 
 main :: IO ()
